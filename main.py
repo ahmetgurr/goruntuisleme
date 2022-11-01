aec380ff6acaa13e0c0027ae276f7b7de4348e4c
Ahmet GÜR <92628011+ahmetgurr@users.noreply.github.com>
2022-11-01 20:28:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
-aslan = cv2.imread("aslan.png")
-cv2.imshow("aslan.png",aslan)
 
-aslan_gri=cv2.imread("aslan.png",0)
-cv2.imshow("aslan.png",aslan_gri)
+def Image_Inversion (Image):
 
-cv2.waitKey()
+    Yukseklik = Image.shape[0]
+    Genislik = Image.shape[1]
+    Kanallar = Image.shape[2]
 
-arrSize=256
-arrRange=(0,256)
+    Boyut = (Yukseklik, Genislik, Kanallar)
 
-Hist = np.zeros(256)
+    yeni_foto = np.zeros(Boyut, np.uint8)
 
-[w,h]=aslan_gri.shape
+    for x in range(0, Yukseklik):
+        for y in range(0, Genislik):
+            for c in range(0, Kanallar):
+                yeni_foto[x, y, c] = 255 - Image[x, y, c]
+    return yeni_foto
 
-for u in range (0,w):
-   for v in range (0,h):
-       index=aslan_gri[u,v]
-       Hist[index]+=1
 
+def main():
+
+    orijinal_foto = cv.imread("elephant.jpg")
+
+    cevirilmis_foto = Image_Inversion(orijinal_foto)
+
+
+    cv.imwrite("cevirilmis_foto.png", cevirilmis_foto)
+    cv.imshow("cevirilmis_foto.png", cevirilmis_foto)
+    cv.waitKey(0)
+
+if __name__ == '__main__':
+    main()
 
